58bfa6719129509785b1dd5c322b766075c2b85e
MaxJamal <106280074+MaxJamal@users.noreply.github.com>
2022-12-05 18:37:16 +0300

    Create utils2.py

---
+++ utils2.py
@@ -0,0 +1,37 @@
+import requests
+import json
+from config import keys
+
+class ConvertionException(Exception):
+    pass
+
+# @bot.message_handler(content_types=['voice', ])
+# def heandle_voice_messages(message: telebot.types.Message):
+#     bot.send_message(message.chat.id, "А у тебя красивый голос")
+
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise ConvertionException(f'Нельзя перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
