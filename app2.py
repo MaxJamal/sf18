3f5241f0af0a4080eef730308d37fd82d891c6f9
MaxJamal <106280074+MaxJamal@users.noreply.github.com>
2022-12-05 18:36:06 +0300

    Create app2.py

---
+++ app2.py
@@ -0,0 +1,42 @@
+import telebot
+from config import keys, TOKEN
+from utils import CryptoConverter, ConvertionException
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def start_help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<Имя валюты> \<в какую валюту> \
+\ <количество переводимой валюты>\n Увидеть список всех доступных валют: /values'
+    bot.send_message(message.chat.id, f'{message.chat.username}, {text}')
+
+@bot.message_handler(commands=['values', ])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys:
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) > 3:
+            raise ConvertionException('Слишком много параметров.')
+        elif len(values) < 3:
+            raise ConvertionException('Недостаточно параметров.')
+
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote.lower(), base.lower(), amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя:\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду. Ошибка сервера:\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {float(total_base) * float(amount)}'
+        bot.send_message(message.chat.id, text)
+# Чтобы запустить бота, нужно воспользоваться методом polling.
+
+bot.polling(none_stop=True)
+# Параметр none_stop=True говорит, что бот должен стараться не прекращать работу при возникновении каких-либо ошибок.
