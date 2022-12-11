19b0c0a9ca2dad1f7ef419381819acd2dadcf54f
MaxJamal <106280074+MaxJamal@users.noreply.github.com>
2022-12-11 11:26:20 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -6,8 +6,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def start_help(message: telebot.types.Message):
-    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<Имя валюты> \<в какую валюту> \
-\ <количество переводимой валюты>\n Увидеть список всех доступных валют: /values'
+    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты цену которой он хочет узнать> \n<имя валюты в которой надо узнать цену первой валюты> \n<количество первой валюты>\n Увидеть список всех доступных валют: /values'
     bot.send_message(message.chat.id, f'{message.chat.username}, {text}')
 
 @bot.message_handler(commands=['values', ])
